88e89a47c7281276920801efc2c9be9da9ad3c24
Logon Aluno <logonrmlocal@fiap.com.br>
2023-11-07 13:09:47 -0300

    ac

--- ecocombustiveis.js
+++ ecocombustiveis.js
@@ -21,7 +21,7 @@ const usuarioSchema = new mongoose.Schema({
     email : {type : String, required : true},
     senha : {type : String}
 });
-const Usuario = mongoose.model("Usuario",  combustivelSchema);
+const Usuario = mongoose.model("Usuario",  usuarioSchema);
 
 //criando a segunda model
 const combustivelSchema = new mongoose.Schema({
@@ -34,7 +34,7 @@ const combustivelSchema = new mongoose.Schema({
 const combustivel = mongoose.model("combustivel", combustivelSchema);
 
 //configurando os roteamentos
-app.post("/cadastrousuario", async(req, res)=>{
+app.post("/cadastroUsuario", async(req, res)=>{
     const email = req.body.email;
     const senha = req.body.senha;
 
@@ -44,6 +44,19 @@ app.post("/cadastrousuario", async(req, res)=>{
         senha : senha,
     })
 
+    const emailExiste = await Usuario.findOne({email:email})
+
+    if(emailExiste) {
+        return res.status(400).json({error : "Email Já Existe!"})
+    }
+    
+    if(!email) {
+        return res.status(400).json({error : "Email não preenchido!"})
+    }
+    if(!senha) {
+        return  res.status(400).json({error : "Senha não preenchida!"})
+    }
+
     try{
         const newUsuario = await usuario.save();
         res.json({error : null, msg : "Cadastro ok", usuarioId : newUsuario._id});
@@ -52,7 +65,7 @@ app.post("/cadastrousuario", async(req, res)=>{
     }
 });
 
-app.post("/cadastrocombustivel", async(req, res)=>{
+app.post("/cadastroEcoCombustiveis", async(req, res)=>{
     const id_combustivel = req.body.id_combustivel;
     const descricao = req.body.descricao;
     const tipo = req.body.tipo;
@@ -60,7 +73,7 @@ app.post("/cadastrocombustivel", async(req, res)=>{
     const litrosEmEstoque = req.body.litrosEmEstoque
 
     //mandando para banco
-    const combustivel = new combustivel({
+    const Comb = new combustivel({
         id_combustivel : id_combustivel,
         descricao : descricao,
         tipo : tipo,
@@ -68,22 +81,51 @@ app.post("/cadastrocombustivel", async(req, res)=>{
         litrosEmEstoque : litrosEmEstoque
     })
 
+    const idexiste = await combustivel.findOne({id_combustivel:id_combustivel})
+
+    if(idexiste) {
+        return res.status(400).json({error : "Id Já Existe!"})
+    }
+
+    if(!id_combustivel) {
+        return res.status(400).json({error : "Id não preenchida!"})
+    }
+    if(!descricao) {
+        return  res.status(400).json({error : "Descrição não preenchida!"})
+    }
+    if(!litrosEmEstoque) {
+        return res.status(400).json({error : "Litros em estoque não preenchido!"})
+    }
+    if(!dataRecebimento) {
+        return  res.status(400).json({error : "Data não preenchida!"})
+    }
+    if(!tipo) {
+        return  res.status(400).json({error : "Senha não preenchida!"})
+    }
+
+    if(litrosEmEstoque > 63) {
+        return res.status(400).json({error : "Limite de litros atingido!"})
+    }
+    if(litrosEmEstoque < 0) {
+        return res.status(400).json({error : "Quantidade de litros deve ser entre 0 e 63."})
+    }
+
     try{
-        const combustivel = await combustivel.save();
-        res.json({error : null, msg : "Cadastro ok", combustivellId : newcombustivel._id});
+        const newComb = await Comb.save();
+        res.json({error : null, msg : "Cadastro ok", combustivellId : newComb._id});
     } catch(error){
         res.status(400).json({error});
     }
 });
 
 //rota para o get de cadastro
-app.get("/cadastrousuario", async(req, res)=>{
+app.get("/cadastroUsuario", async(req, res)=>{
     res.sendFile(__dirname +"/cadastrousuario.html");
 })
 
 //rota para o get de cadastro
-app.get("/cadastrocombustivel", async(req, res)=>{
-    res.sendFile(__dirname +"/cadastrocombustivel.html");
+app.get("/cadastroEcoCombustiveis", async(req, res)=>{
+    res.sendFile(__dirname +"/cadastroEcoCombustiveis.html");
 })
 
 //rota raiz - inw
